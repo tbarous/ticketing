e583c222ab7e9a586836c1ee91ada11f2bc041f9
Node1 <tasosbarous@gmail.com>
2022-03-25 21:33:18 +0200

    p

--- auth/src/middlewares/require-auth.ts
+++ auth/src/middlewares/require-auth.ts
@@ -1,11 +1,12 @@
 import {NextFunction, Request, Response} from "express";
+import {NotAuthorizedError} from "../errors/not-authorized-error";
 
-export const currentUser = (
+export const requireAuth = (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    if (!req.session?.jwt) {
-        return next();
+    if (!req.currentUser) {
+        throw new NotAuthorizedError();
     }
 }
