d8f4e03df5461dc6b8c9bacb089c6c658814b6dc
Node1 <tasosbarous@gmail.com>
2022-03-25 20:06:39 +0200

    p

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express, {Request, Response} from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req: Request, res: Response) => {
-    
+    req.session = null;
+
+    res.send({});
 })
 
 export {router as signoutRouter};
