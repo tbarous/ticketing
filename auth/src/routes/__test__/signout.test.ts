0a40e7ffcb4f0ae1df366d87ece8ce4c42b5024c
Node1 <tasosbarous@gmail.com>
2022-03-25 22:53:18 +0200

    dw

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -1,2 +1,19 @@
 import request from "supertest";
-import
+import {app} from "../../app";
+
+it("Clears the cookie after signing out", async () => {
+    await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.com",
+            password: "12345"
+        })
+        .expect(201);
+
+    const response = await request(app)
+        .post("/api/users/signout")
+        .send({})
+        .expect(200);
+
+    console.log(response.get("Set-Cookie"));
+});
