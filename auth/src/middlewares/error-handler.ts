495a45671be2177ca2ed1d3be2ee277f9e4d1e17
Node1 <tasosbarous@gmail.com>
2022-03-25 19:59:22 +0200

    p

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -8,7 +8,7 @@ export const errorHandler = (
     next: NextFunction
 ) => {
     if (err instanceof CustomError) {
-        res.status(err.statusCode).send({errors: err.serializeErrors()});
+        return res.status(err.statusCode).send({errors: err.serializeErrors()});
     }
 
     res.status(400).send({
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-    res.send("Hello!!!");
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+    if (!req.session?.jwt) {
+        return res.send({currentUser: null});
+    }
+
+    try {
+        const payload = jwt.verify(
+            req.session.jwt,
+            process.env.JWT_KEY!
+        );
+    } catch (err) {
+        res.send()
+    }
+
 })
 
 export {router as currentUserRouter};
