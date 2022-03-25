231fe18fe1bfe68c31b79789d415054642743c27
Node1 <tasosbarous@gmail.com>
2022-03-25 16:25:57 +0200

    p

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,9 +1,30 @@
+import {scrypt, randomBytes} from "crypto";
+import {promisify} from "util";
+
+const scryptAsync = promisify(scrypt);
+
 export class Password {
-    static toHash(password: string) {
-        
+    static async toHash(password: string) {
+        const salt = Password.getSalt();
+
+        const buf = (await Password.getBuffer(password, salt) as Buffer);
+
+        return `${buf.toString("hex")}.${salt}`;
     }
 
-    static compare(storedPassword: string, suppliedPassword: string) {
+    static async compare(storedPassword: string, suppliedPassword: string) {
+        const [hashedPassword, salt] = storedPassword.split(".");
 
+        const buf = (await Password.getBuffer(suppliedPassword, salt) as Buffer);
+
+        return buf.toString("hex") === hashedPassword;
+    }
+
+    static getBuffer(password: string, salt: string) {
+        return scryptAsync(password, salt, 64);
+    }
+
+    static getSalt() {
+        return randomBytes(8).toString("hex");
     }
 }
