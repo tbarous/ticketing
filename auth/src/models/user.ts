f3a5ab4023662593bd8bb4179e83e1a6a77d9d8c
Node1 <tasosbarous@gmail.com>
2022-03-25 19:24:47 +0200

    p

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -33,8 +33,13 @@ const userSchema = new mongoose.Schema({
 }, {
     toJSON: {
         transform(doc, ret) {
+            ret.id = ret._id;
+
+            delete ret._id;
             delete ret.password;
-        }
+            delete ret.__v;
+        },
+        versionKey: false
     }
 });
 
