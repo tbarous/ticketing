7f1db4ef278f3d463f9aa8aba4e35ae9a3c5c653
Node1 <tasosbarous@gmail.com>
2022-03-25 14:27:07 +0200

    Init

---
+++ auth/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import {json} from "body-parser";
+import {currentUserRouter} from "./routes/current-user";
+import {signinRouter} from "./routes/signin";
+import {signupRouter} from "./routes/signup";
+import {signoutRouter} from "./routes/signout";
+import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
+
+const app = express();
+
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all("*", async () => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000!");
+})
