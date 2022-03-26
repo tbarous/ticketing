7b7828e1b5df36c5937bdb3984572eff9946d1a8
Node1 <tasosbarous@gmail.com>
2022-03-26 15:32:30 +0200

    p

--- tickets/src/routes/__test__/create.test.ts
+++ tickets/src/routes/__test__/create.test.ts
@@ -27,11 +27,41 @@ it("Returns a status other than 401 if the user is signed in", async () => {
 });
 
 it("It returns an error if an invalid title is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", getCookie())
+        .send({
+            title: "",
+            price: "10"
+        })
+        .expect(400);
 
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", getCookie())
+        .send({
+            price: "10"
+        })
+        .expect(400);
 });
 
 it("It returns an error if an invalid price is provided", async () => {
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", getCookie())
+        .send({
+            title: "Title",
+            price: -10
+        })
+        .expect(400);
 
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", getCookie())
+        .send({
+            title: "Title"
+        })
+        .expect(400);
 });
 
 it("It creates a ticket with valid inputs", async () => {
