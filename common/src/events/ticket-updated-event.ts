f039e8d613860baeb2a542bbe6582f021efff16a
Node1 <tasosbarous@gmail.com>
2022-03-27 00:42:43 +0200

    d

--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -1,7 +1,7 @@
 import {Subjects} from "./subjects";
 
 export interface TicketCreatedEvent {
-    subject: Subjects.TicketCreated,
+    subject: Subjects.TicketUpdated,
     data: {
         id: string,
         title: string,
