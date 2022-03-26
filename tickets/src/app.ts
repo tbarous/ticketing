7acb7cb315d9b063574201c7f9d1f6d9c7db8af7
Node1 <tasosbarous@gmail.com>
2022-03-26 15:06:43 +0200

    p

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import "express-async-errors";
 import {json} from "body-parser";
 import cookieSession from "cookie-session";
 import {errorHandler, NotFoundError} from "@tbarous/common";
+import {createTicketRouter} from "./routes/create";
 
 const app = express();
 
@@ -10,10 +11,14 @@ app.set("trust proxy", true);
 
 app.use(json());
 
-app.use(cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== "test"
-}))
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== "test"
+    })
+);
+
+app.use(createTicketRouter);
 
 app.all("*", async (req, res) => {
     throw new NotFoundError();
