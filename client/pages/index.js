7d6e29dafef245d824990348754a376b58e7cb86
Node1 <tasosbarous@gmail.com>
2022-03-26 11:49:53 +0200

    p

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({currentUser}) => {
     console.log(currentUser);
@@ -8,23 +8,12 @@ const LandingPage = ({currentUser}) => {
     )
 }
 
-LandingPage.getInitialProps = async ({req}) => {
-    if (typeof window === "undefined") {
-        // we are on the server and need to send request
-        // to ingress nginx
-        const {data} = await axios.get("http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser", {
-            headers: req.headers
-        });
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
 
-        return data;
-    } else {
-        // we are on the browser
-        const {data} = await axios.get("/api/users/currentuser");
+    const {data} = await client.get("/api/users/currentuser");
 
-        return data;
-    }
-
-    return {};
+    return data;
 }
 
 export default LandingPage;
