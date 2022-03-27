e125e74a43a8c467c76b6aa401bf4ba8fff32f33
Node1 <tasosbarous@gmail.com>
2022-03-27 20:21:16 +0300

    Updates

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,12 +1,16 @@
 import {Subjects} from "./subjects";
+import {OrderStatus} from "./types/order-status";
 
 export interface OrderCreatedEvent {
     subject: Subjects.OrderCreated,
     data: {
-        ticketId: string,
+        ticket: {
+            id: string,
+            price: number
+        },
         userId: string,
         price: number,
-        expiresAt: Date,
-        orderId: string
+        expiresAt: string
+        status: OrderStatus
     }
 }
