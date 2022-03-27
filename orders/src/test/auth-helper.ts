ea6cb7353a0fda317590ff60fa9ab9bc870559d7
Node1 <tasosbarous@gmail.com>
2022-03-27 18:01:05 +0300

    p

---
+++ orders/src/test/auth-helper.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import {app} from "../app";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+export const getCookie = () => {
+    // Build a JWT payload. {id, email}
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: "test@test.com"
+    }
+
+    // Create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+    // Build session object. {jwt: TOKEN}
+    const session = {jwt: token};
+
+    // Turn that session to JSON
+    const sessionJSON = JSON.stringify(session);
+
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString("base64");
+
+    // Return an array including the cookie with the encoded data
+    return [`session=${base64}`];
+}
