cbbecd5337d64ec1ea9115fa7789d705b2b6bbac
Node1 <tasosbarous@gmail.com>
2022-03-27 20:30:17 +0300

    p

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from "express";
 import {NotAuthorizedError, NotFoundError, requireAuth} from "@tbarous/common";
 import {Order, OrderStatus} from "../models/order";
+import {OrderCancelledPublisher} from "../events/publishers/order-cancelled-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -10,7 +12,7 @@ router.delete(
     async (req: Request, res: Response) => {
         const {orderId} = req.params;
 
-        const order = await Order.findById(orderId);
+        const order = await Order.findById(orderId).populate("ticket");
 
         if (!order) {
             throw new NotFoundError();
@@ -23,6 +25,11 @@ router.delete(
         order.status = OrderStatus.Cancelled;
 
         await order.save();
+
+        await new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order.id,
+            ticket: {id: order.ticket.id}
+        })
 
         res.status(204).send(order);
     }
