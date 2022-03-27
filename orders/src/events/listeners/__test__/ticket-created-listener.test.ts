de072d8e58021a95b8a50231aa4dc515119e21f3
Node1 <tasosbarous@gmail.com>
2022-03-27 23:09:26 +0300

    p

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,36 @@
+import {TicketCreatedListener} from "../ticket-created-listener";
+import {natsWrapper} from "../../../nats-wrapper";
+import {TicketCreatedEvent} from "@tbarous/common";
+import mongoose from "mongoose";
+import {Message} from "node-nats-streaming";
+import {Ticket} from "../../../models/ticket";
+
+const setup = async () => {
+    const listener = new TicketCreatedListener(natsWrapper.client);
+
+    const data: TicketCreatedEvent["data"] = {
+        version: 0,
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: "Concert",
+        price: 10,
+        userId: new mongoose.Types.ObjectId().toHexString()
+    }
+
+    // @ts-ignore
+    const message: Message = {
+        ack: jest.fn()
+    }
+
+    return {listener, data, message};
+}
+
+it("creates and saves a ticket", async () => {
+    const {listener, data, message} = await setup();
+
+    await listener.onMessage(data, message);
+
+    const ticket = await Ticket.findById(data.id);
+
+    expect(ticket).toBeDefined();
+    expect(ticket.title).to
+})
