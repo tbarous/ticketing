03f9152401c5837d29fa608afd1a09a5307c0287
Node1 <tasosbarous@gmail.com>
2022-03-27 15:09:15 +0300

    p

--- tickets/src/routes/create.ts
+++ tickets/src/routes/create.ts
@@ -3,6 +3,7 @@ import {currentUser, requireAuth, validateRequest} from "@tbarous/common";
 import {body} from "express-validator";
 import {Ticket} from "../models/ticket";
 import {TicketCreatedPublisher} from "../events/publishers/ticket-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -26,7 +27,7 @@ router.post(
 
         await ticket.save();
 
-        new TicketCreatedPublisher(client).publish({
+        await new TicketCreatedPublisher(natsWrapper.client).publish({
             id: ticket.id,
             price: ticket.price,
             title: ticket.title,
