03d75f3edbd19a864867ebcc3f81f1024c354920
Node1 <tasosbarous@gmail.com>
2022-03-27 22:52:56 +0300

    p

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,10 +8,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
     subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
 
     async onMessage(data: TicketUpdatedEvent["data"], message: Message): Promise<void> {
-        const ticket = await Ticket.findOne({
-            _id: data.id,
-            version: data.version - 1
-        });
+        const ticket = await Ticket.findByEvent(data);
 
         if (!ticket) {
             throw new Error("Ticket not found");
