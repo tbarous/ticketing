8eece07bd3a552bdc1648402b784d50a9de6cf5c
Node1 <tasosbarous@gmail.com>
2022-03-27 16:27:09 +0300

    p

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,6 +11,18 @@ const start = async () => {
         throw new Error("MONGO_URI must be defined");
     }
 
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error("NATS_CLIENT_ID must be defined");
+    }
+
+    if (!process.env.NATS_URL) {
+        throw new Error("NATS_URL must be defined");
+    }
+
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error("NATS_CLUSTER_ID must be defined");
+    }
+
     try {
         await natsWrapper.connect(
             "ticketing",
