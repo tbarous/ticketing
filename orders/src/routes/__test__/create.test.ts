a46a784df625ffed44b7bc7dbf299bdce56af41d
Node1 <tasosbarous@gmail.com>
2022-03-27 18:28:44 +0300

    p

--- orders/src/routes/__test__/create.test.ts
+++ orders/src/routes/__test__/create.test.ts
@@ -2,9 +2,11 @@ import request from "supertest";
 import {app} from "../../app";
 import mongoose from "mongoose";
 import {getCookie} from "../../test/auth-helper";
+import {Ticket} from "../../models/ticket";
+import {Order, OrderStatus} from "../../models/order";
 
 it("Returns an error if the ticket does not exist", async () => {
-    const ticketId = mongoose.Types.ObjectId();
+    const ticketId = new mongoose.Types.ObjectId()
 
     request(app)
         .post("/api/orders")
@@ -14,7 +16,29 @@ it("Returns an error if the ticket does not exist", async () => {
 });
 
 it("Returns an error if the ticket is already reserved", async () => {
+    const ticket = Ticket.build({
+        title: "Concert",
+        price: 20
+    });
 
+    await ticket.save();
+
+    const order = Order.build({
+        ticket,
+        userId: "dwqdwq",
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    });
+
+    await order.save();
+
+    request(app)
+        .post("/api/orders")
+        .set("Cookie", getCookie())
+        .send({
+            ticketId: ticket.id
+        })
+        .expect(400);
 });
 
 it("Reserves a ticket", async () => {
