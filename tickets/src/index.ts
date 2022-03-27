1d2c7bfe66d0975d63b1103390f07c001142646d
Node1 <tasosbarous@gmail.com>
2022-03-27 15:15:17 +0300

    p

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,7 +12,20 @@ const start = async () => {
     }
 
     try {
-        await natsWrapper.connect("ticketing", "qdwq", "http://nats-srv:4222")
+        await natsWrapper.connect(
+            "ticketing",
+            "qdwq",
+            "http://nats-srv:4222"
+        );
+
+        natsWrapper.client.on("close", () => {
+            console.log("NATS connection closed!");
+
+            process.exit();
+        });
+
+        process.on("SIGINT", () => natsWrapper.client.close());
+        process.on("SIGTERM", () => natsWrapper.client.close());
 
         await mongoose.connect(process.env.MONGO_URI);
 
