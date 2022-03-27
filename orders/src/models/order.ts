d4be47ed95088280629c1b663bce94f57efa6158
Node1 <tasosbarous@gmail.com>
2022-03-27 17:27:16 +0300

    p

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -27,7 +27,8 @@ const orderSchema = new mongoose.Schema({
     status: {
         type: String,
         required: true,
-        enum: Object.values(OrderStatus)
+        enum: Object.values(OrderStatus),
+        default: OrderStatus.Created
     },
     expiresAt: {
         type: mongoose.Schema.Types.Date
