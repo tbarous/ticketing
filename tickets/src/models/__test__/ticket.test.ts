7e7bc1fa298c4bf129bf91e4a8d5ea78454f58e9
Node1 <tasosbarous@gmail.com>
2022-03-27 22:10:39 +0300

    p

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -9,6 +9,21 @@ it("Implements optimistic concurrency control", async () => {
 
     await ticket.save();
 
-    
+    const firstInstance = await Ticket.findById(ticket.id);
 
-})
+    const secondInstance = await Ticket.findById(ticket.id);
+
+    firstInstance!.set({price: 10});
+
+    secondInstance!.set({price: 10});
+
+    await firstInstance!.save();
+
+    try {
+        await secondInstance!.save();
+    } catch (err) {
+        return;
+    }
+
+    throw new Error("Should not reach this point");
+});
