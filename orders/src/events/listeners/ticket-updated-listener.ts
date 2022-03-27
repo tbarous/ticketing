60bbe5ed83db6f5f86337a483cf3096e20a442df
Node1 <tasosbarous@gmail.com>
2022-03-27 21:26:45 +0300

    p

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import {Listener, Subjects, TicketUpdatedEvent} from "@tbarous/common";
+import {Message} from "node-nats-streaming";
+import {queueGroupName} from "./queue-group-name";
+import {Ticket} from "../../models/ticket";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    queueGroupName = queueGroupName;
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+
+    async onMessage(data: TicketUpdatedEvent["data"], message: Message): Promise<void> {
+        const ticket = await Ticket.findById(data.id);
+
+        if (!ticket) {
+            throw new Error("Ticket not found");
+        }
+
+        const {title, price} = data;
+
+        ticket.set({title, price});
+
+        await ticket.save();
+
+        message.ack();
+    }
+}
