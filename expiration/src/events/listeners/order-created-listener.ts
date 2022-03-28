5661b870321209190bd35a47fba114809227ca71
Node1 <tasosbarous@gmail.com>
2022-03-28 10:16:34 +0300

    p

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,12 @@
+import {Listener, OrderCreatedEvent, Subjects} from "@tbarous/common";
+import {queueGroupName} from "./queue-group-name";
+import {Message} from "node-nats-streaming";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    async onMessage(data: OrderCreatedEvent["data"], message: Message): void {
+    }
+
+    queueGroupName = queueGroupName;
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+
+}
