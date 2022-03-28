49e4396f996b8c6ffda69c138859ab0ea8034ba8
Node1 <tasosbarous@gmail.com>
2022-03-28 10:44:49 +0300

    p

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,9 +1,15 @@
 import {Listener, OrderCreatedEvent, Subjects} from "@tbarous/common";
 import {queueGroupName} from "./queue-group-name";
 import {Message} from "node-nats-streaming";
+import {expirationQueue} from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     async onMessage(data: OrderCreatedEvent["data"], message: Message) {
+        await expirationQueue.add({
+            orderId: data.id
+        });
+
+        message.ack();
     }
 
     queueGroupName = queueGroupName;
