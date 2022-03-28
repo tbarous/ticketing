c140dba13e84788acfe63a7ddb6cad1755a3ea17
Node1 <tasosbarous@gmail.com>
2022-03-28 10:54:23 +0300

    p

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,14 @@ import {expirationQueue} from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     async onMessage(data: OrderCreatedEvent["data"], message: Message) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+        console.log(delay)
+
         await expirationQueue.add({
             orderId: data.id
         }, {
-            delay: 10000
+            delay
         });
 
         message.ack();
