a38b916c020afcc236fb8159e3f4b5e260bcf73a
Node1 <tasosbarous@gmail.com>
2022-03-28 09:06:50 +0300

    p

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -3,6 +3,7 @@ import {natsWrapper} from "../../../nats-wrapper";
 import {Ticket} from "../../../models/ticket";
 import {OrderCreatedEvent, OrderStatus} from "@tbarous/common";
 import mongoose from "mongoose";
+import {Message} from "node-nats-streaming";
 
 const setup = async () => {
     const listener = new OrderCreatedListener(natsWrapper.client);
@@ -26,4 +27,23 @@ const setup = async () => {
         status: OrderStatus.Created,
         version: 0
     };
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    }
+
+    return {listener, ticket, data, msg}
 }
+
+it("Sets the orderId of the ticket", async () => {
+    const {listener, ticket, data, msg} = await setup();
+
+    await listener.onMessage(data, msg);
+
+
+});
+
+it("Acks the message", async () => {
+
+})
