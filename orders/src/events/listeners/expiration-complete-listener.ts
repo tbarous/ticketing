766f68ed6d57d78aa85757d0299fc6cf65044289
Node1 <tasosbarous@gmail.com>
2022-03-28 11:07:31 +0300

    p

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,10 +2,12 @@ import {ExpirationCompleteEvent, Listener, Subjects} from "@tbarous/common";
 import {Message} from "node-nats-streaming";
 import {queueGroupName} from "./queue-group-name";
 import {Order, OrderStatus} from "../../models/order";
+import {OrderCancelledPublisher} from "../publishers/order-cancelled-publisher";
+import {natsWrapper} from "../../nats-wrapper";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
     async onMessage(data: ExpirationCompleteEvent["data"], message: Message) {
-        const order = await Order.findById(data.orderId);
+        const order = await Order.findById(data.orderId).populate("ticket");
 
         if (!order) {
             throw new Error("Order not found");
@@ -16,6 +18,14 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
         });
 
         await order.save();
+
+        await new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            ticket: {id: order.ticket.id},
+            version: order.version
+        });
+
+        msg.ack();
     }
 
     queueGroupName = queueGroupName;
