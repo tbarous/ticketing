830f20fef993df69712393cef45a80eed11a6e34
Node1 <tasosbarous@gmail.com>
2022-03-28 09:20:57 +0300

    p

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import {Listener, OrderCreatedEvent, Subjects} from "@tbarous/common";
 import {Message} from "node-nats-streaming";
 import {queueGroupName} from "./queue-group-name";
 import {Ticket} from "../../models/ticket";
+import {TicketUpdatedPublisher} from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     async onMessage(data: OrderCreatedEvent["data"], message: Message) {
@@ -14,6 +15,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
         ticket.set({orderId: data.id});
 
         await ticket.save();
+
+        new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price: ticket.price,
+            title: ticket.title,
+            userId: ticket.userId,
+            orderId: ticket.orderId,
+            version: ticket.version
+        })
 
         message.ack();
     }
