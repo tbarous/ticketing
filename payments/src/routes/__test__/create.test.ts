c4f58027086f0ba799ab652689d5c63dcaeffbb3
Node1 <tasosbarous@gmail.com>
2022-03-28 14:01:55 +0300

    p

--- payments/src/routes/__test__/create.test.ts
+++ payments/src/routes/__test__/create.test.ts
@@ -38,5 +38,24 @@ it("returns 401 on order non belonging to user", async () => {
 })
 
 it("returns 400 on cancelled order", async () => {
+    const userId = new mongoose.Types.ObjectId().toHexString();
 
+    const order = Order.build({
+        id: new mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        userId: userId,
+        price: 20,
+        status: OrderStatus.Cancelled
+    });
+
+    await order.save();
+
+    await request(app)
+        .post("/api/payments")
+        .set("Cookie", getCookie(userId))
+        .send({
+            token: "dqwdqw",
+            orderId: order.id
+        })
+        .expect(400);
 })
